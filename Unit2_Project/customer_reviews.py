a1437e5f6e68b798ff697e2e5e23270670010c08
AdebolaAj <adebola@techexchange.in>
2022-03-08 19:53:05 +0000

    reviews

---
+++ Unit2_Project/customer_reviews.py
@@ -0,0 +1,48 @@
+class Reviews:
+  general_reviews_list = []
+  items_reviews_list =[]
+  def __init__(self):
+    self.general_reviews_dict = {}
+    self.items_reviews_dict = {}
+    
+  def general_reviews(self, name, number, email, cleaninessGrade, serverGrade, treatmentGrade):
+    answer = ""
+    if name == None:
+      return "Invalid Review!/n Name must be written."
+    if number == None and email == None:
+      return "Are you sure you do not want to be contacted?/n Enter yes to continue or No to enter contact info"
+      input(answer)
+      if answer.lower() == "yes":
+        self.general_reviews_dict = {"name": name, "cleaniness grade": cleaninessGrade, "service grade": serverGrade, "treatment grade": treatmentGrade}
+      else: 
+        return "Invalid Review!/n No contact info."
+    else:
+      self.general_reviews_dict = {"name": name, "number": number, "email": email, "cleaniness grade": cleaninessGrade, "service grade": serverGrade, "treatment grade": treatmentGrade}
+    Reviews.general_reviews_list.append(self.general_reviews_dict)
+
+  def get_general_review(self, name):
+    for reviews in Reviews.general_reviews_list:
+      if reviews[name] == name:
+        return reviews
+    return "Customer not found!"
+    
+  def items_review(self, name, number, email, itemstasteGrade, serviceGrade, itemsSatisfactionGrade):
+    answer = ""
+    if name == None:
+      return "Invalid Review!/n Name must be written."
+    if number == None and email == None:
+      return "Are you sure you do not want to be contacted?/n Enter yes to continue or No to enter contact info"
+      input(answer)
+      if answer.lower() == "yes":
+        self.items_reviews_dict  = {"name": name, "cleaniness grade": itemstasteGrade, "service grade": serviceGrade, "treatment grade": itemsSatisfactionGrade}
+      else: 
+        return "Invalid Review!/n No contact info."
+    else:
+      self.items_reviews_dict  = {"name": name, "number": number, "email": email, "cleaniness grade": itemstasteGrade, "service grade": serviceGrade, "treatment grade": itemsSatisfactionGrade}
+    Reviews.items_reviews_list.append(self.items_reviews_dict )
+
+  def get_items_review(self, name):
+    for reviews in Reviews.items_reviews_list:
+      if reviews[name] == name:
+        return reviews
+    return "Customer not found!"
