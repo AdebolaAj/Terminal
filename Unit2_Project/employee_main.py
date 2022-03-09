2ea0a214a8541f318df06896c91bf155a0b7a658
Flores314 <jonathan@techexchange.in>
2022-03-09 19:26:48 +0000

    employee cli and inventory updates

--- Unit2_Project/employee_main.py
+++ Unit2_Project/employee_main.py
@@ -1,15 +1,128 @@
 from giftcards import *
+from inventory import *
 
 running = True
+first_name = input("What is your first name?\n")
+last_name = input("What is your last name?\n")
+
+employee = Inventory(first_name, last_name)
+
+def verify_item(obj):
+
+    while True:
+
+        print("Enter what section you'de like to update an item in:")
+        obj.get_section()
+        section = input()
+
+        if obj.section_valid(section):
+            break
+
+        else:
+            print("That is not a valid section, enter 1 to exit")
+            exit = input()
+            if exit == '1':
+                return None
+
+    while True:
+
+        print("Enter what category you'de like to update an item in:")
+        obj.get_category(section)
+        category = input()
+
+        if obj.category_valid(section, category):
+            break
+
+        else:
+            print("That is not a valid category, enter 1 to exit")
+            exit = input()
+            if exit == '1':
+                return None
+
+    while True:
+
+        print("Enter what item you'de like to update an item in:")
+        obj.get_items(section, category)
+        item = input()
+
+        if obj.item_valid(section, category, item):
+            break
+
+        else:
+            print("That is not a valid item, enter 1 to exit")
+            exit = input()
+            if exit == '1':
+                return None
+
+    return [section, category, item]
+
 
 while running:
-    print("Would you like to set today's trivia answer(1)? Enter 'logout' to logout") #admin accessible operations
-    user_input = input("> ")
 
-    if user_input == "1": 
+    action = input("What would you like to do?\nEnter 1 to set today's trivia answer.\nEnter 2 to update the inventory\nEnter 3 to inquire about an item's stock  in the inventory\nEnter 'logout' to logout\n")
+
+    if action == "1":
         new_trivia_answer = input("What is the answer for today's trivia question? ")
         Giftcard.set_trivia_solution(new_trivia_answer)
-    elif user_input.lower() == "logout":
-        running = False
+
+    elif action == "2":
+        
+        update_action = input("Enter 1 to add existing item stocks to the inventory\nEnter 2 to remove existing item stocks from the inventory\n")
+
+        if update_action == '1':
+
+            while True:
+            
+                updt_itm = verify_item(employee)
+
+                if updt_itm == None:
+                    continue
+
+                else:
+                    
+                    amount = input("How many of this item are you adding?")
+                    print(employee.add_stock(updt_itm[0], updt_itm[1], updt_itm[2], int(amount)))
+
+                finished = input("Enter 1 to add to another item's inventory or 0 to stop\n")
+
+                if finished == 0:
+                    break
+
+        elif update_action == '2':
+            
+            while True:
+            
+                updt_itm = verify_item(employee)
+
+                if updt_itm == None:
+                    continue
+
+                else:
+                    
+                    amount = input("How many of this item are you removing?")
+                    print(employee.remove_stock(updt_itm[0], updt_itm[1], updt_itm[2], int(amount)))
+
+                finished = input("Enter 1 to remove from another item's inventory or 0 to stop\n")
+
+                if finished == 0:
+                    break
+
+        else:
+            print("That is not a valid input")
+
+    elif action =="3":
+        pass
+
+    elif action == "logout":
+        pass
+    
     else:
-        print("Invalid input")
+        print("That is not a valid input")
+
+    #add
+    #remove
+    #check
+
+# while running:
+
+
