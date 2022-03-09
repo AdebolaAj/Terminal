827893976627a8141c0a77d54d6960ae832d1f32
Shady-Rouk <faroukbalogun@techexchange.in>
2022-03-09 19:12:13 +0000

    updating tests

--- Unit2_Project/test_inventory.py
+++ Unit2_Project/test_inventory.py
@@ -20,6 +20,15 @@ class Test_Inventory(unittest.TestCase):
         self.assertFalse(Inventory.overall_item_exists("SAVORY foods", "black coffee", "Cold"))
         self.assertFalse(Inventory.overall_item_exists("snacks", "black coffee", "Hot"))
         self.assertFalse(Inventory.overall_item_exists("beverages", "water", "Cold"))
+    
+    def test_get_section(self):
+        pass
+
+    def test_get_category(self):
+        pass
+
+    def test_get_item(self):
+        pass
 
     def test_section_valid(self):
         self.assertRaises(TypeError, Inventory.section_valid, 260.0)
@@ -62,8 +71,8 @@ class Test_Inventory(unittest.TestCase):
         self.assertRaises(TypeError, self.employee2.add_stock, "sweet foods", 23, "glazed", "4")
         self.assertRaises(ValueError, self.employee1.add_stock, "beverages", "black coffee", "with ice", -5)
 
-        self.employee1.add_stock("beverages", "milkshakes", "chocolate", 30)
-        self.employee2.add_stock("savory foods", "omlettes", "diced onions", 25)
+        self.assertEqual(self.employee1.add_stock("beverages", "milkshakes", "chocolate", 30), "chocolate has been incremented by 30")
+        self.assertEqual(self.employee2.add_stock("savory foods", "omlettes", "diced onions", 25), "diced onions has been incremented by 25")
         self.assertEqual(Inventory.get_item_count("beverages", "milkshakes", "chocolate"), 30)
         self.assertEqual(Inventory.get_item_count("savory foods", "omlettes", "diced onions"), 25)
 
