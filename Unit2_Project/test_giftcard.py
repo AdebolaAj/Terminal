e8b505fc28e5a7567c3b2d7571b6d697533db76a
AdebolaAj <adebola@techexchange.in>
2022-03-09 18:57:57 +0000

    new tests

--- Unit2_Project/test_giftcard.py
+++ Unit2_Project/test_giftcard.py
@@ -9,15 +9,24 @@ class Test_Giftcard(unittest.TestCase):
     
     def test_generate_code(self):
         self.assertIn(self.giftcard1.gift_code, Giftcard.generated_giftcards)
-    
+        self.assertIn(self.giftcard2.gift_code, Giftcard.generated_giftcards)
+        self.assertIn(self.giftcard3.gift_code, Giftcard.generated_giftcards)
+
     def test_authenticate(self):
         self.assertEqual(Giftcard().authenticate(self.giftcard1.gift_code), True)
+        self.assertEqual(Giftcard().authenticate(self.giftcard2.gift_code), True)
+        self.assertEqual(Giftcard().authenticate(self.giftcard3.gift_code), True)
         self.assertRaises(TypeError, Giftcard().authenticate, 123645)
     
     def test_redeem(self):
         Giftcard().redeem(self.giftcard1.gift_code)
+        Giftcard().redeem(self.giftcard2.gift_code)
+        Giftcard().redeem(self.giftcard3.gift_code)
         self.assertEqual(self.giftcard1.redeem_state, True)
 
     def test_set_trivia_answer(self):
         Giftcard.set_trivia_solution("orange")
         self.assertEqual(Giftcard.trivia_solution, "orange")
+
+if __name__ == "__main__":
+    unittest.main()        
