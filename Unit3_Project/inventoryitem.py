335c75378467673fe96487d6c4e8c6dff23645f7
Flores314 <jonathan@techexchange.in>
2022-04-03 22:06:40 +0000

    added error checks for inventoryitem class

--- Unit3_Project/inventoryitem.py
+++ Unit3_Project/inventoryitem.py
@@ -2,16 +2,38 @@ class InventoryItem:
 
     def __init__(self, section, category, item, amount):
 
+        if type(section) != str:
+            raise TypeError("The section must be a string")
+        if type(category) != str:
+            raise TypeError("The category must be a string")
+        if type(item) != str:
+            raise TypeError("The item must be a string")
+        if type(amount) != int:
+            raise TypeError("The amount must be an integer")
+        if amount < 0:
+            raise ValueError("The amount cannot be less than zero")
+
+
         self.section = section
         self.category = category
         self.item = item
         self.amount = amount 
 
     def CanRemoveAmount(self, update_val):
+
+        if type(update_val) != int:
+            raise TypeError("The update value should be an integer")
+        if update_val <= 0:
+            raise ValueError("The update value must be greater than 0")
         return self.amount >= update_val
             
 
     def RemoveAmount(self, update_val):
+
+        if type(update_val) != int:
+            raise TypeError("The update value should be an integer")
+        if update_val <= 0:
+            raise ValueError("The update value must be greater than 0")
 
         if self.CanRemoveAmount(update_val):
             self.amount -= update_val 
@@ -21,6 +43,11 @@ class InventoryItem:
 
 
     def AddAmount(self, update_val):
+
+        if type(update_val) != int:
+            raise TypeError("The update value should be an integer")
+        if update_val <= 0:
+            raise ValueError("The update value must be greater than 0")
 
         self.amount += update_val
 
