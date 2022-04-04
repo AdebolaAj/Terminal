8bbf2a88862cafbb873d37c9daa4d69c69b8cf82
Flores314 <jonathan@techexchange.in>
2022-04-04 02:38:40 +0000

    adding documentation for the inventoryitem class and inventoryitem class' functions in model.py

--- Unit3_Project/inventoryitem.py
+++ Unit3_Project/inventoryitem.py
@@ -1,6 +1,34 @@
 class InventoryItem:
 
+    '''
+    InventoryItem is a class that represents an instance of an item in an inventory. 
+
+    InventoryItem objects can be generated, and each instance has 4 instances:
+    - The section
+    - The category
+    - The item
+    - The amount
+
+    The attributes:
+        section: a string that represents the specific section of an item in the inventory
+        category: a string that represents the specific category of an item in the inventory
+        item: a string that represents the actual name of an item in the inventory 
+        amount: an integer that represents the amount of an item left in the inventory
+    '''    
+
     def __init__(self, section, category, item, amount):
+
+        '''Constructor for the InventoryItem object with a section string, category string, item string, and an amount integer.
+
+        Args:
+            section(string): the section of an item in the inventory (the first layer)
+            category(string): the category of an item in the inventory  (the second layer)
+            item(string): the actual name of an item (the third layer)
+            amount(integer): the count of an item in the inventory
+
+        Returns:
+            no return(void)
+        '''
 
         if type(section) != str:
             raise TypeError("The section must be a string")
@@ -21,6 +49,15 @@ class InventoryItem:
 
     def CanRemoveAmount(self, update_val):
 
+        '''Checks to see if a specified amount of an item can be removed from the inventory
+
+        Args:
+            update_val(int): the amount of an item that has to be removed
+
+        Returns:
+            boolean: True if the number can be removed, False if it cannot be removed.
+        '''
+
         if type(update_val) != int:
             raise TypeError("The update value should be an integer")
         if update_val <= 0:
@@ -29,6 +66,15 @@ class InventoryItem:
             
 
     def RemoveAmount(self, update_val):
+
+        '''Removes a specific amount of an item from the inventory.
+
+        Args:
+            update_val(int): the amount to be removed from an item's amount in the inventory.
+
+        Returns:
+            no return (void)
+        '''
 
         if type(update_val) != int:
             raise TypeError("The update value should be an integer")
@@ -44,6 +90,15 @@ class InventoryItem:
 
     def AddAmount(self, update_val):
 
+        '''Adds a specified amount to the amount of an item in the inventory
+
+        Args:
+            update_val(int): the amount to be added to the item's amount in inventory
+
+        Returns:
+            No return (void)
+        '''
+
         if type(update_val) != int:
             raise TypeError("The update value should be an integer")
         if update_val <= 0:
@@ -52,10 +107,25 @@ class InventoryItem:
         self.amount += update_val
 
     def to_document(self):
+
+        '''Converts an InventoryItem object instance to a dictionary format.
+
+        Returns:
+            dict: A document representation of the InventoryItem object instance.
+        '''
+
         return {'section': self.section, 'category': self.category, 'item': self.item, 'amount': self.amount}
 
     @classmethod
     def from_document(cls, document):
+
+        '''Creates a InventoryItem object instance from values in a dictionary.
+
+        Args:
+            document (dict): The key-value pairs to be used to create the InventoryItem object.
+        Returns:
+            InventoryItem: An InventoryItem object instance built with the values from document.
+        '''
 
         if type(document) != dict:
             raise TypeError("The document must be a dictionary")
